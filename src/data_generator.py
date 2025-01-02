Fix scaling, the results are wrong

src/data_generator.py
@@ -24,10 +24,8 @@ class DataGeneratorEuropean1D:
 
     def get_pde_data(self, n):
         X = self.sampler_2D.random(n=n)
-        # X = qmc.scale(sample, [self.time_range[0], self.S_range[0]], [
-        #              self.time_range[1], self.S_range[1]])
-        # X = np.concatenate([np.random.uniform(*self.time_range, (n, 1)),
-        #                    np.random.uniform(*self.S_range, (n, 1)) + 1e-7], axis=1)
+        X = qmc.scale(X, [self.time_range[0], self.S_range[0]], [
+                      self.time_range[1], self.S_range[1]])
         y = np.zeros((n, 1))  # price
         return X, y
 
@@ -39,10 +37,9 @@ class DataGeneratorEuropean1D:
 
     def get_expiry_time_data(self, n, w=1):
         sample = self.sampler_1D.random(n=int(w*n))
-        # sample = qmc.scale(sample, [self.S_range[0]], [self.S_range[1]])
-        # X = np.concatenate([np.ones((int(r*n), self.time_range[1])),  # all at expiry time
-        #                    np.random.uniform(*self.S_range, (int(r*n), 1)) + 1e-7], axis=1)
-        X = np.concatenate([np.ones((int(w*n), self.time_range[1])),  # all at expiry time
+        sample = qmc.scale(sample, [self.S_range[0]], [self.S_range[1]])
+
+        X = np.concatenate([np.ones((int(w*n), 1))*self.time_range[1],  # all at expiry time
                             sample], axis=1)
         y = self.option_function(X[:, 1]).reshape(-1, 1)
 
@@ -57,24 +54,21 @@ class DataGeneratorEuropean1D:
     def get_boundary_data(self, n, w1=1, w2=1):
         T = self.time_range[-1]
         lower_sample = self.sampler_1D.random(n=int(n*w1))
-        # lower_sample = qmc.scale(
-        #    lower_sample, [self.time_range[0]], [self.time_range[1]])
-        # lower_X = np.concatenate([np.random.uniform(*self.time_range, (int(n*r1), 1)),
-        #                          self.S_range[0] * np.ones((int(n*r1), 1))], axis=1)
+        lower_sample = qmc.scale(
+            lower_sample, [self.time_range[0]], [self.time_range[1]])
+
         lower_X = np.concatenate([lower_sample,
                                   self.S_range[0] * np.ones((int(w1*n), 1))], axis=1)
         lower_y = np.zeros((int(w1*n), 1))
 
         upper_sample = self.sampler_1D.random(n=int(w2*n))
-        # upper_sample = qmc.scale(
-        #    upper_sample, [self.time_range[0]], [self.time_range[1]])
+        upper_sample = qmc.scale(
+            upper_sample, [self.time_range[0]], [self.time_range[1]])
 
-        # upper_X = np.concatenate([np.random.uniform(*self.time_range, (int(r2*n), 1)),
-        #                          self.S_range[-1] * np.ones((int(r2*n), 1))], axis=1)
         upper_X = np.concatenate([upper_sample,
                                   self.S_range[-1] * np.ones((int(w2*n), 1))], axis=1)
-        upper_y = (self.S_range[-1] - self.K*np.exp(-self.r *
-                                                    (T-upper_X[:, 0].reshape(-1)))).reshape(-1, 1)
+        upper_y = self.S_range[-1] - self.K * \
+            np.exp(-self.r * (T-upper_X[:, 0].reshape(-1))).reshape(-1, 1)
 
         return lower_X, lower_y, upper_X, upper_y
 
@@ -214,16 +208,17 @@ class DataGeneratorAmerican1D(DataGeneratorEuropean1D):
         T = self.time_range[-1]
 
         lower_sample = self.sampler_1D.random(n=int(n*w1))
-        # lower_sample = qmc.scale(
-        #    lower_sample, [self.time_range[0]], [self.time_range[1]])
+        lower_sample = qmc.scale(
+            lower_sample, [self.time_range[0]], [self.time_range[1]])
         lower_X = np.concatenate([lower_sample,
                                   self.S_range[0] * np.ones((int(w1*n), 1))], axis=1)
 
-        lower_y = self.K * np.ones((int(n*w1), 1))
+        lower_y = self.K * np.ones((int(n*w1), 1)) * \
+            np.exp(- self.r * (T - lower_X[:, 0]))
 
         upper_sample = self.sampler_1D.random(n=int(w2*n))
-        # upper_sample = qmc.scale(
-        #    upper_sample, [self.time_range[0]], [self.time_range[1]])
+        upper_sample = qmc.scale(
+            upper_sample, [self.time_range[0]], [self.time_range[1]])
 
         upper_X = np.concatenate([upper_sample,
                                   self.S_range[-1] * np.ones((int(w2*n), 1))], axis=1)

src/experiments_american_one_dimensional.py
@@ -20,14 +20,14 @@ config["w_upper"] = 1
 
 config["weight_decay"] = 0
 config["gamma"] = 0.9
-config["scheduler_step"] = 5000
+config["scheduler_step"] = 5_000
 
-config["N_sample"] = 512
+config["N_sample"] = 64
 config["lambda_pde"] = 1
 config["lambda_boundary"] = 1
 config["lambda_expiry"] = 1
-config["update_lambda"] = 500
-config["alpha_lambda"] = 0.9
+# config["update_lambda"] = 1_000_000
+# config["alpha_lambda"] = 0.9
 
 config["K"] = 40
 config["t_range"] = [0, 1]
@@ -39,6 +39,7 @@ config["learning_rate"] = 1e-3
 config["save_model"] = False
 config["save_loss"] = False
 config["N_INPUT"] = 2
+config["epochs_before_validation"] = 30
 
 dataloader = DataGeneratorAmerican1D(
     time_range=config["t_range"], S_range=config["S_range"], K=config["K"], r=config["r"], sigma=config["sigma"], DEVICE=DEVICE)
@@ -54,14 +55,14 @@ test = dataloader.get_analytical_solution(tmp_X[:, 1], tmp_X[:, 0], n=1024)
 np.save("data/test_data_american_1D", test) """
 
 
-""" torch.manual_seed(2026)
+torch.manual_seed(2026)
 np.random.seed(2026)
 try_different_learning_rates(config=config, dataloader=dataloader, PDE=black_scholes_american_1D,
                              filename2="important_results/american_1D/learning_rates_epochs.txt",
                              filename1="important_results/american_1D/learning_rates.txt",
-                             learning_rates=[5e-3, 1e-3, 5e-4], batch_sizes=[64, 128, 256, 512],
-                             validation_data=validation_data, test_data=test_data,  analytical_solution_filename="data/test_data_american_1D.npy", epochs=350_000)
- """
+                             learning_rates=[1e-3], batch_sizes=[8, 16, 24, 32, 48, 64, 128, 256],
+                             validation_data=validation_data, test_data=test_data,  analytical_solution_filename="data/test_data_american_1D.npy", epochs=600_000)
+
 torch.manual_seed(2026)
 np.random.seed(2026)
 try_multiple_activation_functions(config=config, dataloader=dataloader, PDE=black_scholes_american_1D,
@@ -69,4 +70,4 @@ try_multiple_activation_functions(config=config, dataloader=dataloader, PDE=blac
                                   filename2="important_results/american_1D/activation_epochs_1D_american.txt",
                                   activation_functions=[
                                       nn.ReLU, nn.LeakyReLU, nn.Sigmoid, nn.Tanh],
-                                  layers=[2, 4, 6, 8], validation_data=validation_data, test_data=test_data, analytical_solution_filename="data/test_data_american_1D.npy", epochs=400_000)
+                                  layers=[2, 4, 6, 8], validation_data=validation_data, test_data=test_data, analytical_solution_filename="data/test_data_american_1D.npy", epochs=600_000)

src/experiments_european_one_dimensional.py
@@ -1,7 +1,7 @@
 from train import black_scholes_1D, DEVICE
 from train import try_multiple_activation_functions, try_different_learning_rates, try_different_architectures
 from train import train_multiple_times, create_validation_data
-from train import train, computing_the_greeks, trying_weight_decay
+from train import train, computing_the_greeks, trying_weight_decay, try_different_lambdas
 from data_generator import DataGeneratorEuropean1D
 from PINN import PINNforwards
 
@@ -21,19 +21,19 @@ config["w_lower"] = 1
 config["w_upper"] = 1
 
 config["weight_decay"] = 0
-config["gamma"] = 0.9
-config["scheduler_step"] = 5_000
+config["gamma"] = 0.99
+config["scheduler_step"] = 500
 
-config["N_sample"] = 256
+config["N_sample"] = 64
 config["lambda_pde"] = 1
 config["lambda_boundary"] = 1
 config["lambda_expiry"] = 1
-config["update_lambda"] = 1_000_000
-config["alpha_lambda"] = 0.9
+# config["update_lambda"] = 1_000_000
+# config["alpha_lambda"] = 0.9
 
 config["K"] = 40
 config["t_range"] = [0, 1]
-config["S_range"] = [0, 200]
+config["S_range"] = [0, 400]
 config["sigma"] = 0.5
 config["r"] = 0.04
 
@@ -41,6 +41,7 @@ config["learning_rate"] = 1e-3
 config["save_model"] = False
 config["save_loss"] = False
 config["N_INPUT"] = 2
+config["epochs_before_validation"] = 30
 
 dataloader = DataGeneratorEuropean1D(
     time_range=config["t_range"], S_range=config["S_range"], K=config["K"], r=config["r"], sigma=config["sigma"], DEVICE=DEVICE)
@@ -52,40 +53,31 @@ test_data = create_validation_data(
     dataloader=dataloader, N_validation=20_000, config=config)
 
 
-""" torch.manual_seed(2025)
-np.random.seed(2025)
-try_different_learning_rates(config=config, dataloader=dataloader, PDE=black_scholes_1D,
-                             filename1="important_results/european_1D/MSE_lr_1d_european_test.txt", filename2="important_results/european_1D/epoch_lr_1D_european_test.txt",
-                             learning_rates=[7e-3, 5e-3, 2e-3, 1e-3], batch_sizes=[64, 128],
-                             validation_data=validation_data, test_data=test_data, epochs=600_000) """
-
 """ model = PINNforwards(2, 1, 128, 2, nn.Tanh())
 train(model, 10_000, config["learning_rate"], dataloader,
       config, "test", black_scholes_1D, validation_data) """
 
-
-""" torch.manual_seed(2025)
-np.random.seed(2025)
-try_different_learning_rates(config=config, dataloader=dataloader, PDE=black_scholes_1D,
-                             filename1="important_results/european_1D/MSE_lr_no_lambda.txt", filename2="important_results/european_1D/epoch_lr_no_lambda.txt",
-                             learning_rates=[5e-3, 1e-3, 5e-4], batch_sizes=[16, 32, 64, 128, 256],
-                             validation_data=validation_data, test_data=test_data, epochs=400_000) """
-
-
 torch.manual_seed(2025)
 np.random.seed(2025)
 try_different_learning_rates(config=config, dataloader=dataloader, PDE=black_scholes_1D,
+                             filename1="important_results/european_1D/MSE_test.txt", filename2="important_results/european_1D/epoch_test.txt",
+                             learning_rates=[1e-2, 5e-3, 1e-3], batch_sizes=[32, 64, 128, 256],
+                             validation_data=validation_data, test_data=test_data, epochs=300_000)
+
+""" torch.manual_seed(2025)
+np.random.seed(2025)
+try_different_learning_rates(config=config, dataloader=dataloader, PDE=black_scholes_1D,
                              filename1="important_results/european_1D/MSE_lr_first.txt", filename2="important_results/european_1D/epoch_lr_first.txt",
-                             learning_rates=[1e-2,  5e-3, 1e-3, 5e-4], batch_sizes=[32, 64, 128, 256],
-                             validation_data=validation_data, test_data=test_data, epochs=600_000)
+                             learning_rates=[ 1e-2,  5e-3, 1e-3, 5e-4], batch_sizes=[32, 64, 128, 256],
+                             validation_data=validation_data, test_data=test_data, epochs=600_000) """
 
 
 """ torch.manual_seed(2025)
 np.random.seed(2025)
 try_different_learning_rates(config=config, dataloader=dataloader, PDE=black_scholes_1D,
-                             filename1="important_results/european_1D/MSE_lr_1d_european_fine.txt", filename2="important_results/european_1D/epoch_lr_1D_european_fine.txt",
-                             learning_rates=[2e-3, 1e-3, 9e-4], batch_sizes=[64, 96, 128, 192, 256],
-                             validation_data=validation_data, test_data=test_data, epochs=400_000) """
+                             filename1="important_results/european_1D/MSE_lr_fine.txt", filename2="important_results/european_1D/epoch_lr_fine.txt",
+                             learning_rates=[2e-3, 1e-3, 9e-4], batch_sizes=[8, 16, 24, 32, 48, 64, 72],
+                             validation_data=validation_data, test_data=test_data, epochs=600_000) """
 
 
 """ torch.manual_seed(2025)
@@ -95,7 +87,7 @@ try_multiple_activation_functions(config=config, dataloader=dataloader, PDE=blac
                                   filename2="important_results/european_1D/activation_epochs_1D_european.txt",
                                   activation_functions=[
                                       nn.ReLU, nn.LeakyReLU, nn.Sigmoid, nn.Tanh],
-                                  layers=[1, 2, 4, 8], validation_data=validation_data, test_data=test_data, epochs=400_000) """
+                                  layers=[1, 2, 4, 8], validation_data=validation_data, test_data=test_data, epochs=600_000) """
 
 
 """ torch.manual_seed(2025)
@@ -103,45 +95,44 @@ np.random.seed(2025)
 layers = [1, 2, 4, 6, 8]
 nodes = [32, 64, 128, 256, 512, 1024]
 try_different_architectures(config=config, dataloader=dataloader, PDE=black_scholes_1D,
-                            filename1="important_results/european_1D/arc_1D_european.txt", filename2="important_results/european_1D/arc_epochs_1D_european.txt",
-                            layers=layers, nodes=nodes, validation_data=validation_data, test_data=test_data, epochs=350_000) """
+                            filename1="important_results/european_1D/arc_MSE.txt", filename2="important_results/european_1D/arc_epochs.txt",
+                            layers=layers, nodes=nodes, validation_data=validation_data, test_data=test_data, epochs=600_000) """
 
 
 """ trying_weight_decay(config=config, dataloader=dataloader, PDE=black_scholes_1D, test_data=test_data,
                     filename1="important_results/european_1D/wd_MSE.txt", filename2="important_results/european_1D/wd_epochs.txt",
                     weight_decays=[1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 0.0], validation_data=validation_data,
-                    epochs=400_000) """
-
-""" train_multiple_times(seeds=list(range(1, 10 + 1)), layers=5, nodes=256, PDE=black_scholes_1D, filename="test",
-                        nr_of_epochs=350_000, dataloader=dataloader, config=config, validation_data=validation_data) """
+                    epochs=600_000) """
 
 """ torch.manual_seed(2025)
 np.random.seed(2025)
 config["save_model"] = True
 computing_the_greeks(config=config, dataloader=dataloader, PDE=black_scholes_1D,
-                     filename="important_results/european_1d/greeks.txt", validation_data=validation_data, test_data=test_data, epochs=400_000)
+                     filename="important_results/european_1D/greeks.txt", validation_data=validation_data, test_data=test_data, epochs=600_000)
 config["save_model"] = False """
 
 
-# Try large PDE lambda
+""" train_multiple_times(seeds=list(range(1, 20 + 1)), layers=4, nodes=256, PDE=black_scholes_1D, filename="different_loss",
+                     nr_of_epochs=600_000, dataloader=dataloader, config=config, validation_data=validation_data, test_data=test_data) """
+
 """ torch.manual_seed(2025)
 np.random.seed(2025)
-config["update_lambda"] = 1_000_000
+config["scheduler_step"] = 1_000_000
 try_different_learning_rates(config=config, dataloader=dataloader, PDE=black_scholes_1D,
-                             filename1="important_results/european_1D/MSE_lr_1d_european_no_lambda.txt", filename2="important_results/european_1D/epoch_lr_1D_european_no_lambda.txt",
-                             learning_rates=[1e-2, 5e-3, 1e-3, 5e-4], batch_sizes=[32, 64, 128, 256, 512],
-                             validation_data=validation_data, test_data=test_data, epochs=400_000)
-config["update_lambda"] = 500 """
-
-""" train_multiple_times(seeds=list(range(1, 10 + 1)), layers=4, nodes=256, PDE=black_scholes_1D, filename="with_lambda",
-                     nr_of_epochs=400_000, dataloader=dataloader, config=config, validation_data=validation_data) """
-
+                             filename1="important_results/european_1D/MSE_lr_scheduler.txt", filename2="important_results/european_1D/epoch_lr_scheduler.txt",
+                             learning_rates=[1e-2,  5e-3, 1e-3, 5e-4], batch_sizes=[8, 16, 32, 64, 128],
+                             validation_data=validation_data, test_data=test_data, epochs=600_000)
+config["scheduler_step"] = 5_000 """
 
 """ torch.manual_seed(2025)
 np.random.seed(2025)
-config["update_lambda"] = 1_000_000
-config["N_sample"] = 64
-train_multiple_times(seeds=list(range(1, 10 + 1)), layers=4, nodes=256, PDE=black_scholes_1D, filename="no_lambda_64",
-                     nr_of_epochs=400_000, dataloader=dataloader, config=config, validation_data=validation_data)
-config["N_sample"] = 256
-config["update_lambda"] = 500 """
+try_different_lambdas(config=config, dataloader=dataloader, PDE=black_scholes_1D,
+                      filename1="important_results/european_1D/MSE_lr_lambda.txt", filename2="important_results/european_1D/epoch_lr_lambda.txt",
+                      lambdas=[[1, 1, 1],
+                               [10, 1, 1], [1, 10, 1], [1, 1, 10],
+                               [10, 10, 1], [1, 10, 10], [10, 1, 10],
+                               [50, 1, 1], [1, 50, 1], [1, 1, 50],
+                               [50, 50, 1], [1, 50, 50], [50, 1, 50],
+                               [200, 1, 1], [1, 200, 1], [1, 1, 200],
+                               [200, 200, 1], [1, 200, 200], [200, 1, 200]],
+                      validation_data=validation_data, test_data=test_data, epochs=600_000) """

src/train.py
@@ -201,7 +201,7 @@ def train_one_epoch(model, dataloader, loss_function, optimizer, config, loss_hi
     mse_upper = loss_function(upper_y_tensor, upper_y_pred)
 
     # Loss for boundary conditions
-    # We have to divide by 2 to get the MSE of the boundary condition
+    # We have to divide by 2 to get the MSE of the boundary condition as we both upper and lower
     loss_boundary = config["lambda_expiry"] * mse_expiry + \
         config["lambda_boundary"] * (mse_lower + mse_upper)/2
 
@@ -209,6 +209,7 @@ def train_one_epoch(model, dataloader, loss_function, optimizer, config, loss_hi
     X1, y1 = dataloader.get_pde_data_tensor(N_sample)
 
     X1_scaled = dataloader.normalize(X1)
+
     y1_hat = model(X1_scaled)
 
     bs_pde = PDE(y1_hat, X1, config)
@@ -236,7 +237,7 @@ def train_one_epoch(model, dataloader, loss_function, optimizer, config, loss_hi
     loss_history["loss_lower"].append(mse_lower.item())
     loss_history["loss_upper"].append(mse_upper.item())
 
-    if config["epoch"] % config["update_lambda"] == 0:
+    """ if config["epoch"] % config["update_lambda"] == 0:
         # We have to divide by 2 to get the MSE of the boundary condition
         mse_boundary = (mse_lower.item() + mse_upper.item())/2
 
@@ -257,15 +258,14 @@ def train_one_epoch(model, dataloader, loss_function, optimizer, config, loss_hi
             config["lambda_pde"] + (1 - alpha) * new_lambda_pde
 
         for lambda_name in ["lambda_pde", "lambda_boundary", "lambda_expiry"]:
-            loss_history[lambda_name].append(config[lambda_name])
+            loss_history[lambda_name].append(config[lambda_name]) """
 
 
-def train(model, nr_of_epochs: int, learning_rate: float, dataloader, config: dict, filename: str, PDE, validation_data: dict = {},
-          epochs_before_validation: int = 30):
-
-    config["lambda_pde"] = 1
-    config["lambda_boundary"] = 1
-    config["lambda_expiry"] = 1
+def train(model, nr_of_epochs: int, learning_rate: float, dataloader, config: dict, filename: str, PDE, validation_data: dict = {}):
+    epochs_before_validation = config["epochs_before_validation"]
+    # config["lambda_pde"] = 1
+    # config["lambda_boundary"] = 1
+    # config["lambda_expiry"] = 1
 
     optimizer = torch.optim.Adam(
         model.parameters(), lr=learning_rate, weight_decay=config["weight_decay"])
@@ -276,9 +276,6 @@ def train(model, nr_of_epochs: int, learning_rate: float, dataloader, config: di
                      "loss_pde", "loss_expiry", "loss_lower", "loss_upper"]
     loss_history = {i: [] for i in types_of_loss}
     loss_history_validation = {i: [] for i in types_of_loss}
-
-    for lambda_name in ["lambda_pde", "lambda_boundary", "lambda_expiry"]:
-        loss_history[lambda_name] = [config[lambda_name]]
 
     loss_function = nn.MSELoss()
     best_validation = float("inf")
@@ -363,20 +360,15 @@ def train(model, nr_of_epochs: int, learning_rate: float, dataloader, config: di
         (nr_of_epochs // epochs_before_validation, len(types_of_loss)))
     loss_array = np.zeros(
         (nr_of_epochs, len(types_of_loss)))
-    lambda_values = np.zeros((nr_of_epochs // config["update_lambda"] + 1, 3))
 
     for i, name in enumerate(types_of_loss):
         validation_array[:, i] = loss_history_validation[name]
         loss_array[:, i] = loss_history[name]
 
-    for i, lambda_name in enumerate(["lambda_pde", "lambda_boundary", "lambda_expiry"]):
-        lambda_values[:, i] = loss_history[lambda_name]
-
     if config["save_loss"]:
         np.save("results/loss_" + filename, loss_array)
         np.save("results/validation_" +
                 filename, validation_array)
-        np.save("results/lambda_values_" + filename, lambda_values)
 
     if config["save_model"]:
         torch.save(
@@ -610,15 +602,41 @@ def try_different_architectures(config: dict, dataloader, PDE, filename1: str, f
 
 
 def train_multiple_times(seeds: list[int], layers: int, nodes: int, PDE, filename: str,
-                         nr_of_epochs: int, dataloader, config: dict, validation_data: dict):
+                         nr_of_epochs: int, dataloader, config: dict, validation_data: dict, test_data: dict, analytical_solution_filename: str = None):
     cur_config = config.copy()
     cur_config["save_loss"] = True
 
-    results_train = np.zeros((len(seeds), nr_of_epochs))
-    results_val = np.zeros((len(seeds), nr_of_epochs // 30))
-    results_lambda_pde = np.zeros((len(seeds), nr_of_epochs // 500 + 1))
-    results_lambda_boundary = np.zeros((len(seeds), nr_of_epochs // 500 + 1))
-    results_lambda_expiry = np.zeros((len(seeds), nr_of_epochs // 500 + 1))
+    types_of_loss = ["total_loss", "loss_boundary",
+                     "loss_pde", "loss_expiry", "loss_lower", "loss_upper"]
+
+    results_train = np.zeros((len(seeds), nr_of_epochs, len(types_of_loss)))
+    results_val = np.zeros(
+        (len(seeds), nr_of_epochs // config["epochs_before_validation"], len(types_of_loss)))
+
+    mse_data = np.zeros(len(seeds))
+
+    X1_test = test_data["X1_validation"]
+    X1_test_scaled = test_data["X1_validation_scaled"]
+
+    expiry_x_tensor_test = test_data["expiry_x_tensor_validation_scaled"]
+    expiry_y_tensor_test = test_data["expiry_y_tensor_validation"]
+
+    lower_x_tensor_test = test_data["lower_x_tensor_validation_scaled"]
+    lower_y_tensor_test = test_data["lower_y_tensor_validation"]
+
+    upper_x_tensor_test = test_data["upper_x_tensor_validation_scaled"]
+    upper_y_tensor_test = test_data["upper_y_tensor_validation"]
+
+    if analytical_solution_filename is None:
+        analytical_solution = dataloader.get_analytical_solution(
+            X1_test[:, 1], X1_test[:, 0]).cpu().detach().numpy()
+    else:
+        analytical_solution = np.load(analytical_solution_filename)
+
+    analytical_solution = analytical_solution.reshape(
+        analytical_solution.shape[0], -1)
+
+    MSE = nn.MSELoss()
 
     for i, seed in enumerate(seeds):
         torch.manual_seed(seed)
@@ -630,17 +648,41 @@ def train_multiple_times(seeds: list[int], layers: int, nodes: int, PDE, filenam
 
         cur_train = np.load(f"results/loss_{seed}.npy")
         cur_val = np.load(f"results/validation_{seed}.npy")
-        cur_lambda = np.load(f"results/lambda_values_{seed}.npy")
 
-        results_train[i] = cur_train[:, 0]
-        results_val[i] = cur_val[:, 0]
-        results_lambda_pde[i] = cur_lambda[:, 0]
-        results_lambda_boundary[i] = cur_lambda[:, 1]
-        results_lambda_expiry[i] = cur_lambda[:, 2]
+        results_train[i] = cur_train
+        results_val[i] = cur_val
+
+        model.train(False)
+        model.eval()
+
+        with torch.no_grad():
+            predicted_pde = model(X1_test_scaled).cpu().detach().numpy()
+            predicted_expiry = model(expiry_x_tensor_test)
+            predicted_lower = model(lower_x_tensor_test)
+            predicted_upper = model(upper_x_tensor_test)
+
+        total_number_of_elements = 0
+        mse_data[i] = np.square(np.subtract(
+            analytical_solution, predicted_pde)).mean() * analytical_solution.size
+        total_number_of_elements += analytical_solution.size
+
+        mse_data[i] += MSE(expiry_y_tensor_test,
+                           predicted_expiry).item() * torch.numel(predicted_expiry)
+        total_number_of_elements += torch.numel(predicted_expiry)
+
+        mse_data[i] += MSE(lower_y_tensor_test,
+                           predicted_lower).item()*torch.numel(predicted_lower)
+        total_number_of_elements += torch.numel(predicted_lower)
+
+        mse_data[i] += MSE(upper_y_tensor_test,
+                           predicted_upper).item() * torch.numel(predicted_upper)
+        total_number_of_elements += torch.numel(predicted_upper)
+
+        mse_data[i] /= total_number_of_elements
+        mse_data[i] = np.sqrt(mse_data[i])
 
         os.remove(f"results/loss_{seed}.npy")
         os.remove(f"results/validation_{seed}.npy")
-        os.remove(f"results/lambda_values_{seed}.npy")
         print(f"Run {i} / {len(seeds)} done \n")
 
     np.save("results/average_loss_" +
@@ -648,14 +690,7 @@ def train_multiple_times(seeds: list[int], layers: int, nodes: int, PDE, filenam
 
     np.save("results/average_validation_" +
             filename, np.vstack([results_val.mean(axis=0), results_val.std(axis=0)]))
-
-    final_lambdas_mean = np.vstack([results_lambda_pde.mean(
-        axis=0), results_lambda_boundary.mean(axis=0), results_lambda_expiry.mean(axis=0)])
-    final_lambdas_std = np.vstack([results_lambda_pde.std(
-        axis=0), results_lambda_boundary.std(axis=0), results_lambda_expiry.std(axis=0)])
-    final_lambdas = np.vstack([final_lambdas_mean, final_lambdas_std])
-
-    np.save("results/average_lambdas_" + filename, final_lambdas)
+    np.savetxt("results/mse_data_" + filename + ".txt", mse_data)
 
 
 def computing_the_greeks(config: dict, dataloader, PDE, filename: str, validation_data: dict, test_data: dict, epochs: int = 250_000):
@@ -693,13 +728,11 @@ def computing_the_greeks(config: dict, dataloader, PDE, filename: str, validatio
 
     model = PINNforwards(config["N_INPUT"], 1, 256, 4)
     model.load_state_dict(torch.load("models/greeks.pth", weights_only=True))
-    # model.train(True)
-    # best_epoch = train(
-    #    model, epochs, config["learning_rate"], dataloader, config, "greeks", PDE, validation_data)
-    best_epoch = 399720
+    model.train(True)
+    best_epoch = train(
+        model, epochs, config["learning_rate"], dataloader, config, "greeks", PDE, validation_data)
     model.train(False)
     model.eval()
-
     # print(analytical_delta.shape, analytical_gamma.shape, analytical_rho.shape, analytical_theta.shape, analytical_nu.shape)
     delta, gamma, theta, nu, rho = model.estimate_greeks_call(
         X1_test_scaled, X1_test, sigma, T)
@@ -710,11 +743,11 @@ def computing_the_greeks(config: dict, dataloader, PDE, filename: str, validatio
     nu = nu.cpu().detach().numpy()
     rho = rho.cpu().detach().numpy()
 
-    plt.plot(rho.flatten())
+    """ plt.plot(rho.flatten())
     plt.savefig("plots/pred_rho.png")
     plt.clf()
     plt.plot(analytical_rho.flatten())
-    plt.savefig("plots/rho.png")
+    plt.savefig("plots/rho.png") """
     # print(np.max(analytical_delta), np.max(analytical_gamma), np.max(analytical_theta), np.max(analytical_nu), np.max(analytical_rho))
     # print(np.min(analytical_delta), np.min(analytical_gamma), np.min(analytical_theta), np.min(analytical_nu), np.min(analytical_rho))
 
@@ -802,6 +835,85 @@ def trying_weight_decay(config: dict, dataloader, PDE, filename1: str, filename2
 
         print(f"Trial {i} /  {len(weight_decays)} Done")
         print("RMSE", mse_data[0, i])
+    np.savetxt(filename1, mse_data)
+    np.savetxt(filename2, epoch_data)
+
+
+def try_different_lambdas(config: dict, dataloader, PDE, filename1: str, filename2: str, lambdas: list[list[float]], validation_data: dict, test_data: dict, analytical_solution_filename: str = None, epochs: int = 600_000):
+    cur_config = copy.deepcopy(config)
+
+    X1_test = test_data["X1_validation"]
+    X1_test_scaled = test_data["X1_validation_scaled"]
+
+    expiry_x_tensor_test = test_data["expiry_x_tensor_validation_scaled"]
+    expiry_y_tensor_test = test_data["expiry_y_tensor_validation"]
+
+    lower_x_tensor_test = test_data["lower_x_tensor_validation_scaled"]
+    lower_y_tensor_test = test_data["lower_y_tensor_validation"]
+
+    upper_x_tensor_test = test_data["upper_x_tensor_validation_scaled"]
+    upper_y_tensor_test = test_data["upper_y_tensor_validation"]
+
+    if analytical_solution_filename is None:
+        analytical_solution = dataloader.get_analytical_solution(
+            X1_test[:, 1], X1_test[:, 0]).cpu().detach().numpy()
+    else:
+        analytical_solution = np.load(analytical_solution_filename)
+
+    analytical_solution = analytical_solution.reshape(
+        analytical_solution.shape[0], -1)
+
+    epoch_data = np.zeros(len(lambdas))
+    mse_data = np.zeros(len(lambdas))
+
+    model = PINNforwards(config["N_INPUT"], 1, 256, 4)
+    start_model = copy.deepcopy(model.state_dict())
+
+    MSE = nn.MSELoss()
+    for i, cur in enumerate(lambdas):
+
+        config["lambda_pde"] = cur[0]
+        config["lambda_boundary"] = cur[1]
+        config["lambda_expiry"] = cur[2]
+
+        model.load_state_dict(start_model)
+        model.train(True)
+        best_epoch = train(
+            model, epochs, cur_config["learning_rate"], dataloader, cur_config, "", PDE, validation_data)
+
+        model.train(False)
+        model.eval()
+
+        with torch.no_grad():
+            predicted_pde = model(X1_test_scaled).cpu().detach().numpy()
+            predicted_expiry = model(expiry_x_tensor_test)
+            predicted_lower = model(lower_x_tensor_test)
+            predicted_upper = model(upper_x_tensor_test)
+
+        total_number_of_elements = 0
+        mse_data[i] = np.square(np.subtract(
+            analytical_solution, predicted_pde)).mean() * analytical_solution.size
+        total_number_of_elements += analytical_solution.size
+
+        mse_data[i] += MSE(expiry_y_tensor_test,
+                           predicted_expiry).item() * torch.numel(predicted_expiry)
+        total_number_of_elements += torch.numel(predicted_expiry)
+
+        mse_data[i] += MSE(lower_y_tensor_test,
+                           predicted_lower).item()*torch.numel(predicted_lower)
+        total_number_of_elements += torch.numel(predicted_lower)
+
+        mse_data[i] += MSE(upper_y_tensor_test,
+                           predicted_upper).item() * torch.numel(predicted_upper)
+        total_number_of_elements += torch.numel(predicted_upper)
+
+        mse_data[i] /= total_number_of_elements
+        mse_data[i] = np.sqrt(mse_data[i])
+
+        epoch_data[i] = best_epoch
+
+        print(f"Trial {i} Done")
+        print("RMSE", mse_data[i])
     np.savetxt(filename1, mse_data)
     np.savetxt(filename2, epoch_data)
 